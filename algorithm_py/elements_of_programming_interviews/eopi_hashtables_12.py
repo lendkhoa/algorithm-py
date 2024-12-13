0e555ca37345cc81bf82d62788e1b75f003f935f
Khoa Le <khoale.inbox@gmail.com>
2024-12-12 21:03:10 -0600

    add hash table problems

---
+++ algorithm_py/elements_of_programming_interviews/eopi_hashtables_12.py
@@ -0,0 +1,54 @@
+import collections
+"""
+12.1 Can from a palindrome permutation
+"""
+def can_form_palindrome(s: str):
+    print(collections.Counter(s))
+    print(collections.Counter(s).values())
+    print(sum(v % 2 for v in collections.Counter(s).values()))
+    return sum(v % 2 for v in collections.Counter(s).values()) <= 1
+
+
+can_form_palindrome('edified')
+
+"""
+Check if characters from string can be use to anonymize letter
+Time: O(m+n) m, n are length of letter and magazine
+Space: O(c) c is number of distinct characters in letter
+"""
+def is_letter_constructible_from_magazine(letter_text: str, magazine_text: str) -> bool:
+    return (not collections.Counter(letter_text) - collections.Counter(magazine_text))
+
+"""
+ISBN cache, LRU cache
+"""
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.price_table = collections.OrderedDict()
+        self.capacity = capacity
+    
+    def lookup(self, isbn):
+        if isbn not in self.price_table:
+            return -1
+        price = self.price_table.pop(isbn)
+        self.price_table[isbn] = price
+        return price
+    
+    def insert(self, isbn):
+        if isbn not in self.price_table:
+            return -1
+
+        price = self.price_table.pop(isbn)
+        self.price_table[isbn] = price
+        return price
+    
+    def insert(self, isbn, price):
+        if isbn in self.price_table:
+            price = self.price_table.pop(isbn)
+        elif self.capacity <= len(self.price_table):
+            # remove the first item (oldest) that was inserted
+            self.price_table.popitem(last=False)  
+            self.price_table[isbn] = price
+        return price
+
